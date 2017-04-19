b5b8343354e7f4a591a6e630f1af5425f7e65342
Kndarp Patel <kndarppatel@gmail.com>
2017-04-19 23:06:52 +0530

    Added Logging and other middleware

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var app = express();
+
+//Utilities
+const logger = require('./../utilities/logger');
+
+//Routes
 const api = require('./routes/api.js');
 
+
+app.use(require('morgan')({"stream": logger.stream}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',api);
 
 app.listen(3000, function () {
   console.log('cric-fantasy-league backend running on 3000')
 })
+
+module.exports = app;
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,20 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const cricapi = require('node-cricapi');
+const logger = require('./../../utilities/logger');
 
 // GET /  --- returns message
 router.get('/',function (req,res) {
+  logger.info("Received request for api:GET / ")
   res.send("Call /:id for ball by ball response");
+  logger.info("Exiting api:GET /");
 });
 
 
 // GET /:id --- returns ball by ball response for given game id.
 router.get('/:id',function (req,res) {
+  logger.info("Received request for GET /:id ")
   var id = req.params.id;
   cricapi.ballByBall(id, function(_bbb) {
   			var bbb = JSON.parse(_bbb, null, 2);
         res.send(bbb);
   		});
+  logger.info("Exiting api:GET /");
 });
 
 module.exports = router;
